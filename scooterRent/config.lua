0472a3dd184fd2008e96d4ab24e2477f879ac555
Matota <72810000+mat0ta@users.noreply.github.com>
2020-11-09 15:39:32 +0100

    Add files via upload

---
+++ scooterRent/config.lua
@@ -0,0 +1,38 @@
+Config = {}
+
+Config.PlateText = 'SCOOTER' 
+
+Config.MaxNoJob = 1 
+Config.MaxJob = 3
+
+Config.DistanseSpawn = 50.0 
+Config.DistanseLock = 5.0 
+
+Config.CarLock = true 
+
+Config.VehiclePositions = {            
+   -- 🛵 Alquiler Gratuito 🛵
+	{x = -1010.71, y = -2696.58, z = 13.00, h = 81.69, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1010.06, y = -2694.96, z = 13.00, h = 84.48, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1009.44, y = -2693.55, z = 13.00, h = 76.99, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1008.89, y = -2692.15, z = 13.00, h = 80.65, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1007.13, y = -2688.77, z = 13.00, h = 79.70, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1005.89, y = -2687.23, z = 13.00, h = 81.82, model = 'serv_electricscooter', price = false, job = 'none'},
+	
+	{x = -1020.50, y = -2690.93, z = 13.00, h = 223.85, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1019.57, y = -2689.42, z = 13.00, h = 225.41, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1018.65, y = -2687.41, z = 13.00, h = 225.44, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1017.69, y = -2685.72, z = 13.00, h = 222.39, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1016.08, y = -2682.52, z = 13.00, h = 227.78, model = 'serv_electricscooter', price = false, job = 'none'},
+	{x = -1015.16, y = -2681.00, z = 13.00, h = 222.00, model = 'serv_electricscooter', price = false, job = 'none'},
+
+  -- Example: 🚙 rent a car for money 🚙
+   {x = -238.48, y = 6196.10, z = 30.48, h = 134.24, model = 'serv_electricscooter', price = 3, job = 'none'},
+   {x = -240.77, y = 6199.06, z = 30.48, h = 134.24, model = 'serv_electricscooter', price = 6, job = 'none'},
+   {x = -235.99, y = 6193.85, z = 30.48, h = 134.24, model = 'serv_electricscooter', price = 6, job = 'none'},
+   {x = -243.37, y = 6201.38, z = 30.48, h = 134.24, model = 'serv_electricscooter', price = 6, job = 'none'},
+   {x = -245.52, y = 6203.73, z = 30.48, h = 134.24, model = 'serv_electricscooter', price = 10, job = 'none'},
+   {x = -247.97, y = 6206.30, z = 30.48, h = 134.24, model = 'serv_electricscooter', price = 12, job = 'none'},
+
+
+}	
